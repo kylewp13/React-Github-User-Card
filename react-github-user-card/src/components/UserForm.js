a000c2fb1837332c6705b09e2d93fd4887c7551b
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2019-12-04 09:37:30 -0500

    added friendscard user card and user form for search

---
+++ react-github-user-card/src/components/UserForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        search: ''
+    };
+  }
+
+
+  handleChanges = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addNewUser(this.state.search);
+    this.setState({
+        search: ''
+    });
+  };
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <input
+            type="text"
+            placeholder="Search for a GitHub user"
+            value={this.search}
+            name="search"
+            onChange={this.handleChanges}
+            />
+            <button>Search</button>
+        </form>
+    );
+  }
+}
+
+export default UserForm;
---
+++ react-github-user-card/src/components/friendCard.js
@@ -0,0 +1,19 @@
+  import React from "react";
+
+const FollowersCard = props => {
+  return (
+    <div className="card">
+      <img src={props.user.avatar_url} alt="avatar" />
+      <div className="card-info">
+        <p className="username">{props.user.login}</p>
+        <p>
+          profile: <a href="{props.user.html_url}">{props.user.html_url}</a>
+        </p>
+        <p>Followers: {props.user.followers}</p>
+        <p>Following: {props.user.following}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FollowersCard;
